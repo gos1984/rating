5a0fb92f47e46acf0d3fc5e00da4ca47470da060
Oleg Gayvoronskiy <gos1984@list.ru>
2019-03-06 16:26:43 +0300

    Last version

---
+++ app/view/home/experts.php
@@ -0,0 +1,115 @@
+<section id="experts">
+	<div class="container">
+		<h2>Ведущие эксперты</h2>
+		<div class="row ">
+			<div class="col-12 col-sm-6 col-md-4 expert">
+				<div class="image"><img src="<?php echo PATH_TPL ?>img/experts/expert1.jpg" alt=""></div>
+				<h3>Морозов С.П.</h3>
+				<p class="small">профессор, д.м.н., главный внештатный специалист по лучевой и инструментальной диагностике Минздрава РФ по ЦФО РФ, главный внештатный специалист по лучевой диагностике г. Москвы, председатель МРО РОРР, директор ГБУЗ «Научно-практический клинический центр диагностики и телемедицинских технологий ДЗМ»</p>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4 expert">
+				<div class="image"><img src="<?php echo PATH_TPL ?>img/experts/expert2.jpg" alt=""></div>
+				<h3>Ветшева Н.Н.</h3>
+				<p class="small">д.м.н., заместитель директора по медицинской части ГБУЗ «Научно-практический клинический центр диагностики и телемедицинских технологий ДЗМ»</p>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4 expert">
+				<div class="image"><img src="<?php echo PATH_TPL ?>img/experts/expert3.jpg" alt=""></div>
+				<h3>Владзимирский А.В.</h3>
+				<p class="small">д.м.н., заместитель директора по научной работе ГБУЗ «Научно-практический клинический центр диагностики и телемедицинских технологий ДЗМ»</p>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4 expert">
+				<div class="image"><img src="<?php echo PATH_TPL ?>img/experts/expert4.jpg" alt=""></div>
+				<h3>Трофименко И.А.</h3>
+				<p class="small">к.м.н., заместитель председателя МРО РОРР, руководитель учебно-консультативного отдела ГБУЗ «Научно-практический клинический центр диагностики и телемедицинских технологий Департамента здравоохранения города Москвы»</p>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4 expert">
+				<div class="image"><img src="<?php echo PATH_TPL ?>img/experts/expert5.jpg" alt=""></div>
+				<h3>Ледихова Н.В.</h3>
+				<p class="small">врач-рентгенолог, заведующий консультативным отделением ГБУЗ «Научно-практический клинический центр диагностики и телемедицинских технологий Департамента здравоохранения города Москвы»</p>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4 expert">
+				<div class="image"><img src="<?php echo PATH_TPL ?>img/experts/expert6.jpg" alt=""></div>
+				<h3>Гусева Е. Б.</h3>
+				<p class="small">врач-рентгенолог ГБУЗ «Научно-практический клинический центр диагностики и телемедицинских технологий Департамента здравоохранения города Москвы»</p>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="experts1">
+	<div class="container">
+		<h2>Эксперты</h2>
+		<div class="row">
+			<div class="col-12">
+				<p>Куратор группы экспертов:</p>
+				<p><i>Васильев Ю.А.</i>, к.м.н., старший научный сотрудник ГБУЗ «Научно-практический клинический центр диагностики и телемедицинских технологий ДЗМ»</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-md-6 item d-flex content-justify-between">
+				<div class="descr order-2 order-md-1">
+					<h3>Эксперты по КТ, МРТ, Рентген, Флюорография, Денситометрия:</h3>
+					<p class="light"><i>Подтетенев Д.С.</i>, врач – рентгенолог, заведующий отделением лучевой диагностики «Клиника К+3» </p>
+					<p class="light"><i>Карпов С.С.</i>, заведующий отделением рентгенологической диагностики ГБУЗ «ГП 220 ДЗМ» </p>
+					<p class="light"><i>Васильев Ю.А.</i>, к.м.н., старший научный сотрудник ГБУЗ «Научно-практический клинический центр диагностики и телемедицинских технологий ДЗМ»</p>
+				</div>
+				<div class="image order-1 order-md-2">
+					<img src="<?php echo PATH_TPL ?>img/spec1.svg" alt="">
+				</div>
+			</div>
+			<div class="col-12 col-md-6 item d-flex content-justify-between">
+				<div class="descr order-2 order-md-1">
+					<h3>Эксперт по маммографии:</h3>
+					<p class="light"><i>Пучкова О.С.</i>, врач-рентгенолог отделения лучевой диагностики ФГАУ ЛРЦ Минздрава России</p>
+				</div>
+				<div class="image order-1 order-md-2">
+					<img src="<?php echo PATH_TPL ?>img/spec2.svg" alt="">
+				</div>
+			</div>
+			<div class="col-12 col-md-6 item d-flex content-justify-between">
+				<div class="descr order-2 order-md-1">
+					<h3>Эксперт по УЗИ:</h3>
+					<p class="light"><i>Костенко Е.А.</i>, врач ультразвуковой диагностики ГБУЗ «Научно-практический клинический центр диагностики и телемедицинских технологий ДЗМ»</p>
+				</div>
+				<div class="image order-1 order-md-2">
+					<img src="<?php echo PATH_TPL ?>img/spec3.svg" alt="">
+				</div>
+			</div>
+			<div class="col-12 col-md-6 item d-flex content-justify-between">
+				<div class="descr order-2 order-md-1">
+					<h3>Эксперт РНД, ПЭТ:</h3>
+					<p class="light"><i>Смолярчук М.Я.</i>, заместитель по радионуклидной диагностике главного внештатного специалиста по лучевой диагностике ДЗМ, врач-радиолог «Научно-практический клинический центр диагностики и телемедицинских технологий ДЗМ»</p>
+				</div>
+				<div class="image order-1 order-md-2">
+					<img src="<?php echo PATH_TPL ?>img/spec4.svg" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="sec5">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h2>Методология</h2>
+				<p class="center">Для формирования рейтинга была разработана уникальная методология, в полной мере отвечающая российским требованиям. С подробным описанием вы можете ознакомиться по <a href="<?php echo PATH_TPL ?>img/methodology.pdf" target="_blank">ссылке</a></p>
+			</div>
+		</div>
+	</div>
+</section>
+<hr/>
+<section id="sec6">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="content"></div>
+				<h2>Примите участие в рейтинге отделений лучевой диагностики<br/>прямо сейчас!</h2>
+				<p class="center">Заявите о себе и займите свое место среди лучших!</p>
+				<?php if (!empty($_SESSION['id'])): ?>
+					<a href="/quests" class="button">Принять участие</a>
+					<?php else: ?>
+						<a href="#account" class="fancybox button">Принять участие</a>
+					<?php endif ?>
+				</div>
+			</div>
+		</div>
+	</section>
